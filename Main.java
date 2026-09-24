b5e84558e07e4eb01e5bc6b8067cddcd414bfa64
Erick Figueroa <erickfigueroa0314@gmail.com>
2026-09-24 10:15:42 -0700

    made edits

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ public class Main
 {
 	public static void main(String[] args) {
 		//print "Hello GitHub"
+		System.out.println("Hello Github");
 
 		//print your name
+		System.out.println("Erick Figueroa")
 
 		//make any other edits to this file you want, get creative!
 	}
